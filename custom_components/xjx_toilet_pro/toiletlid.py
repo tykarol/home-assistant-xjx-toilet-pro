63c1867b64b3b8840d9c4636072a07f62d6e77b6
Karol Tyka <madlymanpl@gmail.com>
2021-04-10 23:23:28 +0200

    Add version in manifest

--- custom_components/xjx_toilet_pro/toiletlid.py
+++ custom_components/xjx_toilet_pro/toiletlid.py
@@ -18,14 +18,53 @@ import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Xiaomi Whale Smart Toilet Cover"
-DOMAIN = "xjx_toilet_pro"
 DATA_KEY = "xjx_toilet_pro"
+
+DOMAIN = "xjx_toilet_pro"
+CONF_MODEL = "model"
+MODEL_TOILETLID_XJX_TOILET_PRO = "xjx.toilet.pro"
+
+AVAILABLE_ATTRIBUTES_TOILETLID = [
+    "seating",
+    "air_filter",
+    "led",
+    "self_clean",
+]
+
+AVAILABLE_PROPERTIES = {
+    MODEL_TOILETLID_XJX_TOILET_PRO: [
+        # "fan_temp", # timeout
+        # "left_day", # => [1109] for 109 days left in app
+        # "massage", # timeout
+        # "moving", # timeout
+        "seating",
+        # "seat_temp" # => [0]
+        "status_airfilter",
+        "status_led",
+        "status_selfclean",
+        # "status_tunwash", # => [0, 2, 3, 2, 0, 0]
+        # "status_warmdry", # => [0, 2]
+        # "status_womenwash", # => [0, 2, 2, 2, 0, 0]
+        # "water_pos_t", # timeout
+        # "water_pos_w", # timeout
+        # "water_strong_t", # timeout
+        # "water_strong_w", # timeout
+        # "water_temp_t", # timeout
+        # "water_temp_w", # timeout
+        # "fw_ver", # => [1021]
+    ]
+}
+
+STATE_OCCUPIED = "occupied"
+
+SUCCESS = ["ok"]
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_HOST): cv.string,
         vol.Required(CONF_TOKEN): vol.All(cv.string, vol.Length(min=32, max=32)),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_MODEL): vol.In([MODEL_TOILETLID_XJX_TOILET_PRO]),
     },
     extra=vol.ALLOW_EXTRA,
 )
@@ -37,18 +76,6 @@ SERVICE_LED_OFF = "led_on"
 SERVICE_LED_ON = "led_off"
 ATTR_COMMAND = "command"
 ATTR_PARAMS = "params"
-
-ALL_PROPS = [
-    "is_on",
-    "seating",
-    "air_filter",
-    "led",
-    "self_clean",
-]
-
-STATE_OCCUPIED = "occupied"
-
-SUCCESS = ["ok"]
 
 TOILETLID_SERVICE_SCHEMA = vol.Schema({vol.Optional(ATTR_ENTITY_ID): cv.entity_ids})
 SERVICE_SCHEMA_SEND_COMMAND = TOILETLID_SERVICE_SCHEMA.extend(
@@ -77,6 +104,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
     token = config.get(CONF_TOKEN)
+    model = config.get(CONF_MODEL)
 
     # Create handler
     _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
@@ -84,11 +112,13 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     try:
         miio_device = Device(host, token)
         device_info = miio_device.info()
+        model = device_info.model or model or MODEL_TOILETLID_XJX_TOILET_PRO
+        unique_id = "{}-{}".format(model, device_info.mac_address)
     except DeviceException:
         raise PlatformNotReady
 
-    toiletlid = Toiletlid(host, token)
-    device = XiaomiToiletlid(name, toiletlid)
+    toiletlid = Toiletlid(host, token, model=model)
+    device = ToiletlidEntity(name, toiletlid, model, unique_id)
     hass.data[DATA_KEY][host] = device
 
     async_add_entities([device], update_before_add=True)
@@ -120,31 +150,33 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         )
 
 
-class XiaomiToiletlid(Entity):
+class ToiletlidEntity(Entity):
     """Representation of the device."""
 
-    def __init__(self, name, device):
+    def __init__(self, name, device, model, unique_id):
         """Initialize the device handler."""
         self._name = name
         self._device: Toiletlid = device
+        self._model = model
+        self._unique_id = unique_id
 
         self._state = None
         self._available = False
 
         self._state_attrs = {}
         self._state_attrs.update(
-            {attribute: None for attribute in ALL_PROPS}
+            {attribute: None for attribute in AVAILABLE_ATTRIBUTES_TOILETLID }
         )
+
+    @property
+    def unique_id(self) -> str:
+        """Return an unique ID."""
+        return self._unique_id
 
     @property
     def name(self) -> str:
         """Return the name of the device."""
         return self._name
-
-    @property
-    def state(self) -> str:
-        """Return the state."""
-        return STATE_UNAVAILABLE if not self.available else STATE_OCCUPIED if self.is_on else STATE_IDLE
 
     @property
     def available(self):
@@ -155,6 +187,11 @@ class XiaomiToiletlid(Entity):
     def device_state_attributes(self):
         """Return the state attributes of the device."""
         return self._state_attrs
+
+    @property
+    def state(self) -> str:
+        """Return the state."""
+        return STATE_UNAVAILABLE if not self.available else STATE_OCCUPIED if self.is_on else STATE_IDLE
 
     @property
     def icon(self) -> str:
@@ -176,7 +213,7 @@ class XiaomiToiletlid(Entity):
 
             self._available = True
 
-            for key in ALL_PROPS:
+            for key in AVAILABLE_ATTRIBUTES_TOILETLID:
                 value = getattr(state, key)
                 if isinstance(value, enum.Enum):
                     value = value.name
@@ -237,14 +274,6 @@ class ToiletlidStatus:
     def is_on(self) -> bool:
         return self.seating
 
-    # @property
-    # def massage(self) -> bool:
-    #     return bool(int(self.data["massage"]))
-
-    # @property
-    # def moving(self) -> bool:
-    #     return bool(int(self.data["moving"]))
-
     @property
     def seating(self) -> bool:
         return bool(int(self.data["seating"]))
@@ -261,18 +290,6 @@ class ToiletlidStatus:
     def self_clean(self) -> bool:
         return bool(int(self.data["status_selfclean"]))
 
-    # @property
-    # def tun_wash(self) -> bool:
-    #     return bool(int(self.data["status_tunwash"]))
-
-    # @property
-    # def warm_dry(self) -> bool:
-    #     return bool(int(self.data["status_warmdry"]))
-
-    # @property
-    # def women_ash(self) -> bool:
-    #     return bool(int(self.data["status_womenwash"]))
-
 
 class Toiletlid(Device):
     def __init__(
@@ -282,39 +299,22 @@ class Toiletlid(Device):
         start_id: int = 0,
         debug: int = 0,
         lazy_discover: bool = True,
+        model: str = MODEL_TOILETLID_XJX_TOILET_PRO,
     ) -> None:
         super().__init__(ip, token, start_id, debug, lazy_discover)
+
+        if model in AVAILABLE_PROPERTIES:
+            self.model = model
+        else:
+            self.model = MODEL_TOILETLID_XJX_TOILET_PRO
 
     # https://github.com/rytilahti/python-miio/issues/815#issuecomment-761765046
     def status(self) -> ToiletlidStatus:
         """Retrieve properties."""
-        properties = [
-            # "fan_temp", # timeout
-            # "left_day", # => [1109] for 109 days left in app
-            # "massage", # timeout
-            # "moving", # timeout
-            "seating",
-            "status_airfilter",
-            "status_led",
-            "status_selfclean",
-            # "status_tunwash", # => [0, 2, 3, 2, 0, 0]
-            # "status_warmdry", # => [0, 2]
-            # "status_womenwash", # => [0, 2, 2, 2, 0, 0]
-            # "water_pos_t", # timeout
-            # "water_pos_w", # timeout
-            # "water_strong_t", # timeout
-            # "water_strong_w", # timeout
-            # "water_temp_t", # timeout
-            # "water_temp_w", # timeout
-            # "fw_ver", # => [1021]
-        ]
+        properties = AVAILABLE_PROPERTIES[self.model]
         values = self.get_properties(properties, max_properties=1)
 
         return ToiletlidStatus(dict(zip(properties, values)))
-
-    # def raw_command(self, command: str, props: Dict[str, Any] = []):
-    #     """Send raw command."""
-    #     return self.send(command, props)
 
     def set_self_clean(self, state: bool):
         """Turn the self clean on/off."""
